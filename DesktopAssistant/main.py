e17e69df6984f43a540b2aca12682e3c0ecb750b
Sahil Chhoker <chhokersahil435@gmail.com>
2024-01-15 23:02:51 +0530

    Set up the input system and basic commands

--- DesktopAssistant/main.py
+++ DesktopAssistant/main.py
@@ -1,10 +1,36 @@
 import speech_recognition as sr
 import win32com.client as wincom
+import webbrowser
+import openai
+import datetime
 
 def say(text):
     speak.Speak(text)
 
+def TakeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        try:
+            print("Recognizing...")
+            query = r.recognize_google_cloud(audio, language="en-US")
+            print(f"User said : {query}")
+            return query
+        except Exception as e:
+            return "Some error occurred, try again!"
+
 
 if __name__ == "__main__":
     speak = wincom.Dispatch("SAPI.SpVoice")
-    say("Hello i am Sahil AI")
+    while True:
+        print("Listening...")
+        query = TakeCommand()
+        sites = [["youtube", "https://www.youtube.com"]]
+        for site in sites:
+            if f"Open {site[0]}".lower() in query.lower():
+                say(f"Openeing {site[0]} sir...")
+                webbrowser.open(site[1])
+        if "the time" in query:
+            strfTime = datetime.datetime.now().strftime("%H:%M:%S")
+            say(f"Sir the time is {strfTime}")
