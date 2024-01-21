4f364304629990dd3bb178ebdda7a92f431959a0
Sahil Chhoker <chhokersahil435@gmail.com>
2024-01-21 09:45:18 +0530

    added a black screen and a default size

--- ToDoList/main.py
+++ ToDoList/main.py
@@ -4,4 +4,16 @@ import tkinter.messagebox
 window = Tk()
 
 window.title("TO DO List App in Python")
+
+frame_task = Frame(window)
+frame_task.pack()
+
+listbox_task = Listbox(frame_task, bg = "black", fg="white", height=15, width=50, font="Helvetica")
+listbox_task.pack(side=tkinter.LEFT)
+
+
+
+
+
+
 window.mainloop()
