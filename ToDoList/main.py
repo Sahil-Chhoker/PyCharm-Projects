b0ac0afea47e1725cef4de51f501c60fcde39200
Sahil Chhoker <chhokersahil435@gmail.com>
2024-01-21 09:48:55 +0530

    Got a scroll bar

--- ToDoList/main.py
+++ ToDoList/main.py
@@ -11,7 +11,10 @@ frame_task.pack()
 listbox_task = Listbox(frame_task, bg = "black", fg="white", height=15, width=50, font="Helvetica")
 listbox_task.pack(side=tkinter.LEFT)
 
-
+scroolbar_tasks = Scrollbar(frame_task)
+scroolbar_tasks.pack(side=tkinter.RIGHT,fill=tkinter.Y)
+listbox_task.config(yscrollcommand=scroolbar_tasks.set)
+scroolbar_tasks.config(command=listbox_task.yview)
 
 
 
