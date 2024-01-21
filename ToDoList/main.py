1a1a9fe8f800084c133485ab599d40d25c6fae28
Sahil Chhoker <chhokersahil435@gmail.com>
2024-01-21 09:37:50 +0530

    got a simple window going

--- ToDoList/main.py
+++ ToDoList/main.py
@@ -1,30 +1,7 @@
-import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox
-import sqlite3 as sql
+from tkinter import *
+import tkinter.messagebox
 
-def add_task():
-    task_string = input("Write the task : ")
+window = Tk()
 
-    if len(task_string) == 0:
-        messagebox.showinfo("Error", "Field is Empty")
-    # else:
-    #     tasks.append(task_string)
-    #
-    #     the_cursor.execute('insert into tasks values (?)', (task_string,))
-    #     list_update()
-    #     task_field.delete(0, 'end')
-
-def list_update():
-    clear_list()
-    # for task in tasks:
-        # task_listbox.insert('end', task)
-
-
-def clear_list():
-    pass
-    # using the delete method to delete all entries from the list box
-    # task_listbox.delete(0, 'end')
-
-if __name__ == "__main__":
-    tasks = []
+window.title("TO DO List App in Python")
+window.mainloop()
